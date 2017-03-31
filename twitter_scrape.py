bacbe172b115f3a8555c836b52103d24a11f1b64
tbesold <tbesold@fwparker.org>
2017-03-31 18:48:48 -0500

    done

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -5,3 +5,16 @@
 # Use BeautifulSoup and urllib to grab the text contents of the tweets
 # located on the twitter page you chose.  The .text attribute will supply the content of a soup object.
 # Have fun.  Again, nothing explicit. (15pts)
+
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+url = "https://twitter.com/williegdi?lang=en"
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page.read(), "html.parser")
+
+tweets = [x.text.strip() for x in soup.findAll('p', {'class' :"TweetTextSize TweetTextSize--large js-tweet-text tweet-text"})]
+for i in range(len(tweets)):
+    print(tweets[i])
+
--- weather_scrape.py
+++ weather_scrape.py
@@ -8,5 +8,38 @@
 # attributes to pull some parts of the data.
 # (e.g.  Wednesday, March 22: the high temp will be 48 with a low of 35, and a 20% chance of rain). (25pts)
 
+from bs4 import BeautifulSoup
+import urllib.request
 
 url = "https://weather.com/weather/tenday/l/USIL0225:1:US"
+
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page.read(), "html.parser")
+
+my_table = soup.find("table")
+rows = my_table.findAll("tr")
+header = my_table.findAll("th")
+
+header_list = []
+for item in header:
+    header_list.append(item.text.strip())
+print(header_list)
+data_list = []
+
+for i in range(1, len(rows)):
+    cells = rows[i].findAll('td')
+    row_list = []
+    for j in range(len(cells)):
+        row_list.append(cells[j].text.strip())
+    data_list.append(row_list)
+print(data_list)
+
+for i in range(len(data_list)):
+    data_list[i].pop(0)
+    data_list[i].pop(1)
+    data_list[i].pop(-1)
+    data_list[i].pop(-1)
+print(data_list)
+
+for i in range(len(data_list)):
+    print("on", data_list[i][0], "the high/low temperature is", data_list[i][1], "degrees, and the chance of rain is", data_list[i][2])
